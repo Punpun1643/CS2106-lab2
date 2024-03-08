a95f754cf3e4cd9f41af3339d73fff323ab83177
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-03-08 14:29:23 +0800

    Feat: complete part2

--- part2/lab2p2f.c
+++ part2/lab2p2f.c
@@ -15,6 +15,37 @@ int main() {
     // I.e. your program should do the equivalent of ./slow 5 | talk > results.out
     // WITHOUT using | and > from the shell.
     //
+    int fp_out = open("./results.out", O_CREAT | O_WRONLY); 
 
+    int p[2];
+
+    // Create pipe
+    if (pipe(p) < 0) {
+      perror("lab2p2f: ");
+    }
+
+    // Create 2 processes: slow and talk
+    if (fork() != 0) { // parent
+      close(p[0]); // close reading end
+      // copy writing end file descriptor for stdout
+      dup2(p[1], STDOUT_FILENO);
+
+      // execute ./slow 5
+      execlp("./slow", "slow", "5", NULL);
+      close(p[1]);
+      wait(NULL);
+    } else { // child
+      char buffer[128];
+
+      close(p[1]);
+      
+      dup2(p[0], STDIN_FILENO);
+      dup2(fp_out, STDOUT_FILENO);
+
+      execlp("./talk", "talk", (char*) 0);
+
+      close(p[0]);
+      close(fp_out);
+    }
 }
 
